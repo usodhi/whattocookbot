eb317a5a0ffac1d83bc03eecb7b926d65375ed7b
usodhi <61431818+usodhi@users.noreply.github.com>
2021-08-25 00:16:51 +0530

    streaming stuff

--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+import requests
+from Food import get_random_meal
 import tweepy
 from tweepy.error import TweepError
 from dotenv import load_dotenv
@@ -10,18 +12,30 @@ CONSUMER_API_SECRET_KEY = os.getenv('CONSUMER_API_SECRET_KEY')
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
 
+APP_HANDLE = 'whattocookbot'
+
 auth = tweepy.OAuthHandler(CONSUMER_API_KEY, CONSUMER_API_SECRET_KEY)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
+api.get_user()
+
+def on_status(tweet):
+    if APP_HANDLE in map(lambda x: x['screen_name'], tweet.entities['user_mentions']):
+        meal = get_random_meal()
+        # api.media_upload()
+
+def get_image(url):
+    pass
 
 try:
     streamListener = tweepy.StreamListener(api)
+    streamListener.on_connect = lambda: print("Connected!")
     streamListener.on_error = lambda error: print(error)
-    streamListener.on_connect = lambda a: print(a)
+    streamListener.on_status = on_status
+
     stream = tweepy.Stream(api.auth, streamListener)
-    
-    stream.filter(track=['python'])
+    stream.filter(track=['@whattocookbot'])
 
 except TweepError as e:
     print(f"TweepError: \nargs:{e.api_code}\nreason:{e.reason}\nresponse:{e.response}")
